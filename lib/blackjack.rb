09c6b1e1108d72a3f0a7a44011f85bde3d3d53ba
Daniel Ran <danielr1735@gmail.com>
2018-06-19 09:40:41 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,5 +47,10 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  total = 0
+  welcome
+  initial_round
+  until total > 21
+  hit?(total)
+  display_card_total
 end
